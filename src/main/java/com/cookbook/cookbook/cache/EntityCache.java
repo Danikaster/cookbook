d8181f73bbaaf32c2db37053b4cf4e0e43f0c3c2
danique <kiseldaniil25@gmail.com>
2024-03-28 03:19:42 +0300

    added caching

--- src/main/java/com/cookbook/cookbook/cache/EntityCache.java
+++ src/main/java/com/cookbook/cookbook/cache/EntityCache.java
@@ -6,26 +6,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class EntityCache<KEY, VALUE> {
-    private final Map<KEY, VALUE> cache;
+public class EntityCache<K, V> {
+    private final Map<K, V> cache;
     private static final int MAX_SIZE = 100;
 
     public EntityCache() {
         this.cache = new HashMap<>();
     }
 
-    public void put(KEY key, VALUE value) {
+    public void put(K key, V value) {
         if (cache.size() >= MAX_SIZE) {
             cache.clear();
         }
         cache.put(key, value);
     }
 
-    public VALUE get(KEY key) {
+    public V get(K key) {
         return cache.get(key);
     }
 
-    public void remove(KEY key) {
+    public void remove(K key) {
         cache.remove(key);
     }
 
