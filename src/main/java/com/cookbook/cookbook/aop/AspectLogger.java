393ad5a275afa103b0968c06cc56358ab672fa29
danique <kiseldaniil25@gmail.com>
2024-04-17 21:32:32 +0300

    added logging

--- src/main/java/com/cookbook/cookbook/aop/AspectLogger.java
+++ src/main/java/com/cookbook/cookbook/aop/AspectLogger.java
@@ -1,9 +1,31 @@
 package com.cookbook.cookbook.aop;
 
-import org.aspectj.lang.annotation.Aspect;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
+@Log4j2
 public class AspectLogger {
+    @Before("execution(* com.cookbook.cookbook.service.*.*(..))")
+    public final void logBeforeServiceCommand(final JoinPoint joinPoint) {
+        log.info(() -> String.format("Running method %s with args %s", joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs())));
+    }
+
+    @AfterReturning(pointcut = "execution(* com.cookbook.cookbook.service.*.*(..))")
+    public final void logAfterServiceCommand(final JoinPoint joinPoint) {
+        log.info(() -> String.format("Result of %s: success ", joinPoint.getSignature().getName()));
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.cookbook.cookbook.*.*.*(..))", throwing = "exception")
+    public final void logAfterError(final JoinPoint joinPoint, final Exception exception) {
+        log.error(
+                () -> String.format("Error while running %s with args %s: %s", joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()), exception.getMessage()));
+    }
 }
